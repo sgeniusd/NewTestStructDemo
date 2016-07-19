17b0b6e4c8243eef8776932b27d77f58a74f1cd7
songdan <songd@yiche.com>
2016-07-19 11:08:41 +0800

    Initial Commit

---
+++ NewTestStructDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewTestStructDemo
+//
+//  Created by bita on 16/7/19.
+//  Copyright © 2016年 MagicSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewTestStructDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTestStructDemo
+//
+//  Created by bita on 16/7/19.
+//  Copyright © 2016年 MagicSong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
